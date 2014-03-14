bdab8ac4b80ef0bfa16217dbfcf8b3abda64d1ba
kathib <kathibalazs@gmail.com>
2014-03-14 14:37:36 +0100

    Nem változott semmi

--- WPFClient/Form1.Designer.cs
+++ WPFClient/Form1.Designer.cs
@@ -53,7 +53,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 262);
+            this.ClientSize = new System.Drawing.Size(433, 185);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
